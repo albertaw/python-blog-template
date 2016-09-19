8ad8d3de7d0b7c701eddba5dd2154dd29465d9be
Alberta Williams <alberta@cerebromusic.com>
2016-09-19 13:55:58 +0200

    creates dummy posts and shows all posts on /post

--- post/views.py
+++ post/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Post
 # Create your views here.
+
+
 def index(request):
-	return render(request, 'post/index.html', {})
+	posts = Post.objects.order_by('-created_at')
+	context = {'posts': posts}
+	return render(request, 'post/index.html', context)
