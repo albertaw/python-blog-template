7c3974667e928a76a7486321a4b46345059f0948
Alberta Williams <alberta@cerebromusic.com>
2016-09-25 00:16:14 +0200

    adds tests for post

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User
 
 
 class DashboardForm(forms.Form):
-    # use form widgets to customize the fields
-    # https://docs.djangoproject.com/en/1.10/ref/forms/widgets/
-    # username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    # email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     # make fields false to avoid returning errors with empty fields
     username = forms.CharField(required=False, )
     email = forms.CharField(required=False)
--- post/forms.py
+++ post/forms.py
@@ -2,4 +2,8 @@ from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
 
 class PostForm(forms.Form):
+    # use form widgets to customize the fields
+    # https://docs.djangoproject.com/en/1.10/ref/forms/widgets/
+    # username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    # email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control','rows': 5}))
